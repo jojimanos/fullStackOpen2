23a9dd685f3386b7519dfcbc4e0bb353e96fd83c
jojimanos <manosgeorge7@gmail.com>
2023-07-30 15:08:50 +0300

    exercise 2.6

---
+++ exercises_2.6-2.10/phonebook/src/App.js
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      name: newName
+    }
+    setPersons([...persons, newPerson]);
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={(event) => {setNewName(event.target.value)}}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
+    </div>
+  )
+}
+
+export default App
