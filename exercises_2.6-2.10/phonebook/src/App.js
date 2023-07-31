5e577faaa9c98fa163d85e1aaf5cf11e84103578
jojimanos <manosgeorge7@gmail.com>
2023-07-31 10:04:05 +0300

    exercise 2.11

--- exercises_2.6-2.10/phonebook/src/App.js
+++ exercises_2.6-2.10/phonebook/src/App.js
@@ -1,15 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import InputForm from './InputForm'
 import SearchBar from './SearchBar'
 import Numbers from './Numbers'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/persons')
+      .then(response => {
+        console.log('Success');
+        setPersons(response.data)
+      })
+  }, [])
+
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchName, setSearchName] = useState('')
@@ -35,7 +41,6 @@ const App = () => {
   }
 
   let searchPerson = persons.filter((person) => person.name.toLowerCase() === searchName.toLowerCase())
-  // }
 
   console.log(persons, searchPerson)
 
@@ -43,9 +48,9 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <div>
-        <SearchBar searchName={searchName} setSearchName={setSearchName} searchPerson={searchPerson}/>
-        <InputForm newName={newName} newNumber={newNumber} setNewName={setNewName} setNewNumber={setNewNumber} handleSubmit={handleSubmit}/>
-        <Numbers persons={persons}/>
+        <SearchBar searchName={searchName} setSearchName={setSearchName} searchPerson={searchPerson} />
+        <InputForm newName={newName} newNumber={newNumber} setNewName={setNewName} setNewNumber={setNewNumber} handleSubmit={handleSubmit} />
+        <Numbers persons={persons} />
       </div>
     </div>
   )
